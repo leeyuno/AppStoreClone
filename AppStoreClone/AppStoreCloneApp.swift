887184624bee3ddc60d89438e5320b2a2aa89e41
leeyuno <leeyuno@kdiwin.com>
2022-11-10 15:47:19 +0900

    Initial Commit

---
+++ AppStoreClone/AppStoreCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreCloneApp.swift
+//  AppStoreClone
+//
+//  Created by 이윤오 on 2022/11/10.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
